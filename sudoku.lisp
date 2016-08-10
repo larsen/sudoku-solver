436fc06666cfdc6e749d45dddcac81fd3a91a3e1
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-08-10 21:00:09 +0200

    fixes, it now correctly loads the board from a file

--- sudoku.lisp
+++ sudoku.lisp
@@ -12,13 +12,17 @@
 
 (defun load-board (filename)
   (let ((board (make-array '(9 9) :initial-element 'empty))
-        (lines (remove-if (lambda (l) (equal l ""))
-                          (slurp-file filename))))
+        (lines (remove-if
+                (lambda (l) (equal l ""))
+                (slurp-file filename))))
     (loop
        for line in lines
        for y upfrom 0 do
          (loop
             for x upfrom 0 
             for c across (remove #\Space line)
-            do (setf (aref board x y) c)))
+            do (setf (aref board x y)
+                     (if (equal c #\.)
+                         'empty
+                         (parse-integer (make-string 1 :initial-element c))))))
     board))
