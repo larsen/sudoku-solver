a638a116ed1c847d0999db43ef7218a69c21e687
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-08-10 20:12:25 +0200

    Initial import

---
+++ sudoku.asd
@@ -0,0 +1,10 @@
+;;;; sudoku.asd
+
+(asdf:defsystem #:sudoku
+  :description "Describe sudoku here"
+  :author "Your Name <your.name@example.com>"
+  :license "Specify license here"
+  :serial t
+  :components ((:file "package")
+               (:file "sudoku")))
+
---
+++ sudoku.lisp
@@ -0,0 +1,24 @@
+;;;; sudoku.lisp
+
+(in-package #:sudoku)
+
+(defun slurp-file (filename)
+  (let ((in (open filename)))
+    (when in
+      (loop
+         for line = (read-line in nil)
+         while line
+         collect line))))
+
+(defun load-board (filename)
+  (let ((board (make-array '(9 9) :initial-element 'empty))
+        (lines (remove-if (lambda (l) (equal l ""))
+                          (slurp-file filename))))
+    (loop
+       for line in lines
+       for y upfrom 0 do
+         (loop
+            for x upfrom 0 
+            for c across (remove #\Space line)
+            do (setf (aref board x y) c)))
+    board))
